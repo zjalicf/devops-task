843db6ee8d4a2cfdc27e045fc4e302aa268c2664
Filip Zjalic <zjalicf@gmail.com>
2023-04-26 13:27:24 +0200

    added checks, env and proper error handling

--- app.go
+++ app.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-func homePage(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+const (
+    healthzPath  = "/probe/liveness"
+    readinessPath = "/probe/readiness"
+)
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello Goldbach from @Filip")
+}
+
+func healthz(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintln(w, "Healthy!")
+}
+
+func readyz(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintln(w, "Ready!")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc(healthzPath, healthz)
+	http.HandleFunc(readinessPath, readyz)
+
+	port := os.Getenv("PORT")
+	
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
+	log.Printf("Server listening on port %s...", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", homePage)
+	handleRequests()
 }
